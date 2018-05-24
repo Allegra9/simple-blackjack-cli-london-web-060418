ebe48894bded37468127d0a05fa5d2cb104ad695
Allegra <aukseallegra@gmail.com>
2018-05-24 20:59:32 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  card_total = deal_card + deal_card 
-  display_card_total(card_total)
+  first_round = deal_card + deal_card 
+  display_card_total(first_round)
   return card_total
 end
 
